f8a48c3f3b57b910312e69ef2623d8ceaec376ed
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-03-18 13:12:15 +0400

    Initialize Infrastructure layer: - Added ApplicationDbContext with DbSets for all entities - Configured SQL Server connection and Serilog logging in Program.cs - Established correct Project References between API, Infrastructure, Application and Domain

---
+++ Ecommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
